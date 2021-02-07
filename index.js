b5f94e6ee1067545f55897c6392d11d32457e5a6
essennejaye <sjeffers02@gmail.com>
2021-02-06 21:51:57 -0700

    some csv file renders

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const fs = require('fs');
 const processLineByLine = async () => {
     let contactList = [];
 
-    const fileStream = fs.createReadStream('C:/Users/essen/OneDrive/Documents/bootcamp_doc/contacts.csv');
+
+
+    const fileStream = fs.createReadStream('C:/Users/essen/Downloads/user-notes_react.csv');
     // const fileStream = fs.createReadStream('some.txt');
 
     const rl = readline.createInterface({
@@ -16,24 +18,38 @@ const processLineByLine = async () => {
     });
 
     for await (const line of rl) {
-        let contactObj = {};
-        const contactArr = line.split(",");
-        contactObj.contactName = contactArr[0];
-        contactObj.contactLinkedIn = contactArr[1].trim();
-        contactList.push(contactObj);
-    }
-    // console.log(contactList);
 
-    // try {
-        fs.writeFile('./index.html', generatePage(contactList), err => {
-            if (err)
-                throw new Error(err);
-            console.log('Success. Check out index.html');
-        });
-    // }
-    // catch (error) {
-    //     console.log('something happened ' + error);
-    // }
+        const createObj = (contactArr) => {
+            let contactObj = {};
+            contactObj.courseNote = contactArr[0];
+            contactObj.courseCourse = contactArr[1];
+            contactObj.courseModule = contactArr[2];
+            contactObj.courseClip = contactArr[3];
+            contactObj.courseClipTime = contactArr[4];
+            contactList.push(contactObj);
+        }
+        // let contactObj = {};
+        const contactArr = line.split(",");
+        if (contactArr.length === 6) {
+            createObj(contactArr);
+        }
+        // else {
+        //     const tempArr = line.split('",');
+        //     if (tempArr.length === 6) {
+        //         createObj(tempArr);
+        //     }
+        //     // if (tempArr.length === 6) {
+        //     // }
+        // }
+    }
+    console.log(contactList, contactList.length);
+
+
+    // fs.writeFile('./index.html', generatePage(contactList), err => {
+    //     if (err)
+    //         throw new Error(err);
+    //     console.log('Success. Check out index.html');
+    // });
 
 }
 processLineByLine();
